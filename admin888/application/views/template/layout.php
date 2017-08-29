e8ca44a3c00f8c8b2c80406f21bfcb616d04e4c4
wisadev <supachai.wi@gmail.com>
2017-08-29 22:08:43 +0700

    remove

--- admin888/application/views/template/layout.php
+++
@@ -1,5 +0,0 @@
-<?= $this->load->view('template/header'); ?>
-<?php //$this->load->view('template/navigation'); ?>
-<?= $this->load->view($content); ?>
-<?= $this->load->view('template/footer'); ?>
-<?php  // $this->load->view('template/script'); ?>
